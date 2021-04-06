e3694e575620d6a2c12f549686c7197bd798c53d
staffordcian99 <staffordcian99@gmail.com>
2021-04-06 15:54:05 +0000

    Add color change effect when mouse hovers over buttons

--- assets/scripts/game-script.js
+++ assets/scripts/game-script.js
@@ -1,1 +1,8 @@
-console.log("Ready")
+;(function (window, document, undefined) {
+	$(".button-play").mouseenter(function(){
+        $(".button-play").css("background-color", "coral")
+    })
+    $(".button-play").mouseleave(function(){
+        $(".button-play").css("background-color", "rgb(228, 132, 7)")
+    })
+})(window, document);
--- assets/scripts/home-script.js
+++ assets/scripts/home-script.js
@@ -1,1 +1,8 @@
-console.log("home ready")
+;(function (window, document, undefined) {
+	$(".button-play").mouseenter(function(){
+        $(".button-play").css("background-color", "coral")
+    })
+    $(".button-play").mouseleave(function(){
+        $(".button-play").css("background-color", "rgb(228, 132, 7)")
+    })
+})(window, document);
