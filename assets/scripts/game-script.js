78a595df6a947f33b0f56e7cae0c9e2fa113a16e
staffordcian99 <staffordcian99@gmail.com>
2021-04-14 14:15:32 +0000

    add player projectiles and animated projectile movement

--- assets/scripts/game-script.js
+++ assets/scripts/game-script.js
@@ -10,8 +10,12 @@
         $("#score-div").remove()
     });
 
+    //the game 
+
     let canvas = document.querySelector('#myCanvas');
     let can = canvas.getContext('2d');
+
+    //creates player constructor
 
     class Player{
         constructor(x, y, radius, color){
@@ -29,6 +33,72 @@
         }
     };
 
-    let player = new Player(canvas.width / 2 , canvas.height / 2, 5, 'white');
-    player.create();
+    //creates rocket constructor
+
+    class Rocket{
+        constructor(x, y, radius, color, velocity){
+            this.x = x
+            this.y = y
+            this.radius = radius
+            this.color = color
+            this.velocity = velocity
+        }
+
+        create(){
+            can.beginPath()
+            can.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+            can.fillStyle = this.color
+            can.fill()
+        }
+
+        update(){
+            this.create()
+            this.x = this.x + this.velocity.x
+            this.y = this.y + this.velocity.y
+        }
+    };
+
+    //creating the player 
+
+    let player = new Player(canvas.width / 2 , canvas.height / 2, 15, 'white');
+    
+
+    //creating and animating rockets
+    
+    
+    let rockets = [];
+    
+    function animate(){
+        requestAnimationFrame(animate);
+        
+        can.clearRect(0, 0, canvas.width, canvas.height);
+        
+        player.create();
+        
+        rockets.forEach((rocket) => {
+            rocket.update()
+        });
+    };
+
+    addEventListener('click', (event) => {
+       
+        let angle = Math.atan2(
+            event.clientY - canvas.height / 2,
+            event.clientX - canvas.width / 2
+        );
+        
+        let velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        };
+        
+        rockets.push(
+        new Rocket(
+            canvas.width / 2 , canvas.height / 2,
+            2, 'red', velocity
+        ));
+
+    })
+
+    animate();
 })(window, document);
